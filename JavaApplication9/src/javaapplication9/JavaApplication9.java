fd7c775964576a7153bf70a604d4f81c0d7359f7
luisk97 <30986170+luisk97@users.noreply.github.com>
2017-08-13 22:45:53 -0600

    Update JavaApplication9.java

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *ok  you are the fucking boss ok you are the big boss
  */
 package javaapplication9;
 
