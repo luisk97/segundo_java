973704e5a42d007b81843eb4a466bc9dad7450e9
luisk97 <30986170+luisk97@users.noreply.github.com>
2017-08-13 22:31:18 -0600

    Update JavaApplication9.java

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -15,7 +15,7 @@ public class JavaApplication9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here ok you are the mother foker yea nene
     }
     
 }
